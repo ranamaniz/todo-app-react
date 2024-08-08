58e1c7da2cedf99a17c014764e63cf4b957ffece
maniz <alertmanishrana@gmail.com>
2024-08-08 10:32:40 +0545

    feat: add and remove todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+type TODO = {
+  id: number;
+  title: string;
+}[];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoInput, setTodoInput] = useState("");
+
+  const [todos, setTodos] = useState<TODO>([]);
+
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const input = e.currentTarget.value;
+    console.log(input);
+    setTodoInput(input);
+  };
+
+  const handleAddTodo = () => {
+    const id = Math.floor(Date.now() - Math.random() * 1000);
+    // TODO: use uuid later
+
+    const newTodo = { id, title: todoInput };
+
+    console.log(id);
+    console.log(newTodo);
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+    setTodoInput("");
+  };
+
+  const handleRemoveTodo = (todoId: number) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="max-h-screen">
+      <article className="flex justify-Hello tcenter flex-col px-6 py-4 ">
+        <h1 className="text-center text-slate-700">TODO</h1>
+
+        <section className="flex justify-center gap-2 mt-6  ">
+          <input
+            className="w-72 border-slate-500 border-solid border-2 rounded-md p-2"
+            onChange={handleInputChange}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                return handleAddTodo();
+              }
+              return;
+            }}
+            value={todoInput}
+            placeholder="Just do it!"
+          />
+          <button
+            className="bg-blue-500 rounded-md py-2 px-6 text-white"
+            onClick={handleAddTodo}
+          >
+            Add
+          </button>
+        </section>
+
+        <section className="flex justify-center ">
+          <ul className="border-1 border-slate-400 border-solid rounded-md min-h-[calc(100vh_-_200px)] w-96 my-6 px-6 py-4 bg-slate-50 ">
+            {(!todos || todos.length === 0) && (
+              <p className="italic text-slate-800">
+                Add some tasks you would like to do today...
+              </p>
+            )}
+
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
+                className="flex justify-start  items-center gap-2 w-full mb-2 relative"
+              >
+                <input
+                  type="checkbox"
+                  id={todo.id.toString()}
+                  className="mr-2"
+                />
+                <label htmlFor={todo.id.toString()}>{todo.title}</label>
+
+                <button
+                  onClick={() => handleRemoveTodo(todo.id)}
+                  className="w-5 h-5 rounded-[50%] bg-rose-500 text-white flex items-center justify-center absolute right-0 top-1/2 -translate-y-1/2"
+                >
+                  &#x2715;
+                </button>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </article>
+    </main>
+  );
 }
 
-export default App
+export default App;
